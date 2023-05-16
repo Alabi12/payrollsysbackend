eb4082bc1ad852adaa460e45c096cf7cee3e056a
Alabi12 <wolo22001@gmail.com>
2023-05-16 01:04:45 +0000

    setup database for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
-class ApplicationController < ActionController::Base
-end
+class ApplicationController < ActionController::API
+    include ActionController::Helpers
+    include CanCan::ControllerAdditions
+  
+    rescue_from CanCan::AccessDenied do |exception|
+      render json: { error: exception.message }, status: :forbidden
+    end
+  
+    def current_user
+      @current_user ||= User.find_by(authentication_token: request.headers['Authorization'])
+    end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
+  before_create :generate_authentication_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+      class Ability
+        include CanCan::Ability
+        
+        def initialize(user)
+          if user.admin?
+            can :manage, :all
+          else
+            can :read, :all
+            can [:create, :update], [Employee, Salary, PayrollPeriod, PayrollEntry]
+          end
+        end
+      end
+      private
+
+  def generate_authentication_token
+    self.authentication_token = loop do
+      token = SecureRandom.base64(64)
+      break token unless User.exists?(authentication_token: token)
+    end
+  end  
 end
